c9e25c34cacd00c93f502237239c6cb81e8ddbb6
imrdoctor <mhamedfekry0@gmail.com>
2024-07-17 00:52:40 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+"use strict"
 document.addEventListener('DOMContentLoaded', function() {
     const dataTable = document.getElementById('dataTable');
     const dataBody = document.getElementById('dataBody');
